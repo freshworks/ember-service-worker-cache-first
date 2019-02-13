1e64bd41bd43e0472d53fa6328588040af412263
vishnu balaji <vishnu.balaji@freshworks.com>
2019-02-13 11:53:39 +0530

    Splitting for api and assets cache name and patterns

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,11 +1,47 @@
-import { PATTERNS, VERSION } from 'ember-service-worker-cache-first/service-worker/config';
+import {
+  API_PATTERNS,
+  ASSET_PATTERNS,
+  VERSION,
+  API_CACHE_KEY_PREFIX,
+  ASSET_CACHE_KEY_PREFIX
+} from 'ember-service-worker-cache-first/service-worker/config';
 import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
 import { createUrlRegEx, urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
 
-const CACHE_KEY_PREFIX = 'esw-cache-first';
-const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
+const API_CACHE_NAME = `${API_CACHE_KEY_PREFIX}-${VERSION}`;
+const ASSET_CACHE_NAME = `${ASSET_CACHE_KEY_PREFIX}-${VERSION}`;
 
-const PATTERN_REGEX = PATTERNS.map(createUrlRegEx);
+const API_PATTERN_REGEX = API_PATTERNS.map(createUrlRegEx);
+const ASSET_PATTERN_REGEX = ASSET_PATTERNS.map(createUrlRegEx);
+
+const FETCH_DATA = (event, cacheName) => {
+  let request = event.request;
+  event.respondWith(
+    caches.open(cacheName).then((cache) => {
+      return cache.match(request).then((response) => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(request).then((response) => {
+          cache.put(request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+};
+
+
+const CLEAR_API_CACHE = () => {
+  caches.keys().then((cacheNames) => {
+    cacheNames.forEach((cacheName) => {
+      if (cacheName == API_CACHE_NAME) {
+        caches.delete(cacheName);
+      }
+    });
+  });
+};
 
 self.addEventListener('fetch', (event) => {
   let request = event.request;
@@ -13,24 +49,22 @@ self.addEventListener('fetch', (event) => {
     return;
   }
 
-  if (urlMatchesAnyPattern(request.url, PATTERN_REGEX)) {
-    event.respondWith(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.match(request).then((response) => {
-          if (response) {
-            return response;
-          }
+  if (urlMatchesAnyPattern(request.url, ASSET_PATTERN_REGEX)) {
+    FETCH_DATA(event, ASSET_CACHE_NAME);
+  }
 
-          return fetch(request).then((response) => {
-            cache.put(request, response.clone());
-            return response;
-          });
-        })
-      })
-    );
+  if (urlMatchesAnyPattern(request.url, API_PATTERN_REGEX)) {
+    FETCH_DATA(event, API_CACHE_NAME);
   }
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(cleanupCaches(CACHE_KEY_PREFIX, CACHE_NAME));
+  event.waitUntil(cleanupCaches(ASSET_CACHE_KEY_PREFIX, ASSET_CACHE_NAME));
+  event.waitUntil(cleanupCaches(API_CACHE_KEY_PREFIX, API_CACHE_NAME));
+});
+
+self.addEventListener('message', (event) => {
+  if(event.data.type === 'sync') {
+    CLEAR_API_CACHE();
+  }
 });
